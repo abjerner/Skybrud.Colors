a0258d37661a7755cf39cf20fe9ad77138624250
Anders Bjerner <abjerner@skybrud.dk>
2021-04-23 17:15:34 +0200

    Added some XML documentation

--- src/Skybrud.Colors/Wcag/WcagConformanceLevel.cs
+++ src/Skybrud.Colors/Wcag/WcagConformanceLevel.cs
@@ -5,9 +5,22 @@ namespace Skybrud.Colors.Wcag {
     /// Enum class representing the WCAG conformance level.
     /// </summary>
     public enum WcagConformanceLevel {
+        
+        /// <summary>
+        /// Indicates an A level conformance level.
+        /// </summary>
         A,
+
+        /// <summary>
+        /// Indicates an AA level conformance level.
+        /// </summary>
         Aa,
+
+        /// <summary>
+        /// Indicates an AAA level conformance level.
+        /// </summary>
         Aaa
+
     }
 
 }
