cea2e1d0891d97dc482a4bcf46b3030a5ea3dc00
Anders Bjerner <abjerner@skybrud.dk>
2023-09-26 20:32:25 +0200

    Build stuff etc.

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Skybrud.Colors --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
+dotnet build src/Skybrud.Colors --configuration Release /t:rebuild /t:pack -p:PackageOutputPath=../../releases/nuget
